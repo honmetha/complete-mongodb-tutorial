0efbf2113563fafd2f9c9352a472a5e8eb1fd248
Hon <dennis.mejen@gmail.com>
2022-08-02 23:56:13 +0700

    📦 add: 18 - Finding Single Documents

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
@@ -30,4 +31,19 @@ app.get("/books", (req, res) => {
     .catch(() => {
       res.status(500).json({ error: "Could not fetch the documents" });
     });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
 });
