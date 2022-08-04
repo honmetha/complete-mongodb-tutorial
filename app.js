e956640a9e9faf892b8c1c6645db56b9c32f6e76
Hon <dennis.mejen@gmail.com>
2022-08-05 00:45:31 +0700

    📦 add: 21 - Handling DELETE Requests

--- app.js
+++ app.js
@@ -61,3 +61,18 @@ app.post("/books", (req, res) => {
       res.status(500).json({ err: "Could not create a new document" });
     });
 });
+
+app.delete("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not delete the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
