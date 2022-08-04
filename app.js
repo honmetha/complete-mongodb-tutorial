9cbc9aa62c4750e3bef327ce90ebf57ffe409c17
Hon <dennis.mejen@gmail.com>
2022-08-04 23:59:35 +0700

    📦 add: 20 - Handling POST Requests

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
 const app = express();
+app.use(express.json);
 
 // db connection
 let db;
@@ -46,4 +47,17 @@ app.get("/books/:id", (req, res) => {
   } else {
     res.status(500).json({ error: "Not a valid doc id" });
   }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).json({ err: "Could not create a new document" });
+    });
 });
